16e629d56cd51782bd259d574b57d5cb986fbbdf
Chet Printhong <chetthaphong@bunditcenter.com>
2020-07-21 12:47:47 +0700

    wait for DB initializing

--- install.sh
+++ install.sh
@@ -19,12 +19,23 @@ mv wordpress html
 echo "Create a WordPress service."
 docker-compose up -d
 mypassword=$(grep MYSQL_ROOT_PASSWORD docker-compose.yml|awk -F\= '{print $2}')
-docker exec db sh -c "mysql -p$mypassword -e 'create database wordpress;'"
 docker exec web sh -c "chown -R www-data:www-data /var/www/html"
-
-echo "Remove install.sh script."
-rm -f ./install.sh
+docker exec db sh -c "mysql -p$mypassword -e 'create database wordpress;'" >/dev/null 2>&1
+while [ $? -ne 0 ]; do
+  echo "Initilize DB... (This might take a minute)"
+  sleep 20
+  docker exec db sh -c "mysql -p$mypassword -e 'create database wordpress;'" >/dev/null 2>&1
+done
+echo "done..."
 
 echo "====================="
 echo "WordPress site: $(hostname -I|awk '{print "http://"$1":8888"}')"
+echo "[Database info]"
+echo "db:   wordpress"
+echo "host: db"
+echo "user: root"
+echo "pass: $mypassword"
 echo "====================="
+
+echo "Remove install.sh script."
+rm -f ./install.sh
